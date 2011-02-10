fbbd38a8234923fc15e328755ff5d906fcc8aebe
josieshima@gmail.com <josieshima@gmail.com@80012f0e-d4d3-8958-a474-7b48c4e3bd98>
2011-02-10 18:01:11 +0000

    Changes GNU GPL3 licensing.

--- src/ThreadTerminator.java
+++ src/ThreadTerminator.java
@@ -1,6 +1,23 @@
-//package edu.auburn.eng.aci.xtoolss;
-
-
+/*
+ * Copyright 2005 Mike Tinker, Gerry Dozier, Aaron Gerrett, Lauren Goff, 
+ * Mike SanSoucie, and Patrick Hull
+ * Copyright 2011 Joshua Adams
+ * 
+ * This file is part of X-TOOLSS.
+ *
+ * X-TOOLSS is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * X-TOOLSS is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with X-TOOLSS.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 public class ThreadTerminator {
 	public boolean killThread;
